18875a3784dbedb5e0eb549f9516dfa3a7f47d40
Vaibhav-Sharma-01 <thegreatvaibhavsharma.com>
2022-12-15 09:44:51 +0530

    format

--- typescriptdemo/shoppingcart.ts
+++ typescriptdemo/shoppingcart.ts
@@ -1,34 +1,34 @@
-class Product{
-    id: number
-    name: String
-    brand: String
-    price: number
-    constructor(id: number, name: String, brand: String, price: number){
-        this.id = id
-        this.brand = brand
-        this.name = name
-        this.price = price
-    }
+class Product {
+  id: number;
+  name: String;
+  brand: String;
+  price: number;
+  constructor(id: number, name: String, brand: String, price: number) {
+    this.id = id;
+    this.brand = brand;
+    this.name = name;
+    this.price = price;
+  }
 }
 
-class ShoppingCart{
+class ShoppingCart {
+  constructor(public products: any) {}
 
-    constructor(public products: any){
-    }
-
-    calculateCart = () => {
-        let sum = 0
-        this.products.forEach((value: number, key: Product) => {
-            sum += (key.price*value)
-            console.log(`Price of ${value} ${key.brand} ${key.name} is ${key.price*value}`);
-        });
-        console.log(`which makes a total chekout of Rs.${sum}`)
-    }
+  calculateCart = () => {
+    let sum = 0;
+    this.products.forEach((value: number, key: Product) => {
+      sum += key.price * value;
+      console.log(
+        `Price of ${value} ${key.brand} ${key.name} is ${key.price * value}`
+      );
+    });
+    console.log(`which makes a total chekout of Rs.${sum}`);
+  };
 }
 
 let products = new Map<Product, number>();
-products.set(new Product(1,"shirt","denim", 3000), 4)
-products.set(new Product(2,"T-shirt","denim", 2000), 12)
-products.set(new Product(3,"Jeans","denim", 2500), 1)
-let sh = new ShoppingCart(products)
-sh.calculateCart()
+products.set(new Product(1, "shirt", "denim", 3000), 4);
+products.set(new Product(2, "T-shirt", "denim", 2000), 12);
+products.set(new Product(3, "Jeans", "denim", 2500), 1);
+let sh = new ShoppingCart(products);
+sh.calculateCart();
