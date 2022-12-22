cec60b116bed84156fdc2d7ee018225c46db2790
Vaibhav-Sharma-01 <thegreatvaibhavsharma.com>
2022-12-22 17:07:15 +0530

    Structure Improve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PentHouseComponent } from './pent-house/pent-house.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { AnimalListComponent } from './animal-list/animal-list.component';
+import { PentHouseComponent } from './Day1/pent-house/pent-house.component';
+import { WelcomeComponent } from './Day1/welcome/welcome.component';
+import { AnimalListComponent } from './Day2/animal-list/animal-list.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductlistComponent } from './Day2/productlist/productlist.component';
 
 @NgModule({
   declarations: [
