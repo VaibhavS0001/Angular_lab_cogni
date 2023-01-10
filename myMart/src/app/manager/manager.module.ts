ec99cd81b9c9d01dade8b7e2287a424a8eb30092
Vaibhav-Sharma-01 <thegreatvaibhavsharma.com>
2023-01-10 11:13:07 +0530

    myMartApp

---
+++ myMart/src/app/manager/manager.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ManagerHomeComponent } from './manager-home/manager-home.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+
+
+
+@NgModule({
+  declarations: [
+    ManagerHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule
+  ]
+})
+export class ManagerModule { }
